565601f5601ef986a6dec018334f99d350eebb24
Jamalvandi <125701210+Jamalvandi@users.noreply.github.com>
2024-12-25 16:26:19 +0330

    app_name
    add app name for url.py and ulr in base.html

--- website/url.py
+++ website/url.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from website.views import *
+# if had molty app and url name be in more than one place
+app_name = "website"
 urlpatterns = [
-    path('' , indexView),
-    path('about' , aboutView),
-    path('contact' , contactView),
-    path('elements' , elementsView),
+    path('' , indexView, name="index"),
+    path('about' , aboutView, name= "about"),
+    path('contact' , contactView, name= "contact"),
+    path('elements' , elementsView, name="elements"),
 ]
