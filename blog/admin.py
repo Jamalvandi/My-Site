bb7ed931397d7c088d0e1451845a842975d9aef5
Jamalvandi <125701210+Jamalvandi@users.noreply.github.com>
2024-12-31 13:11:36 +0330

    admin
    base admin thing we need in project

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    empty_value_display = "Empty"
+    list_display = ("title","counted_views","state","published_date","created_date")
+    list_filter = ("state",)
+    search_fields = ["title" , "content"]
+    # ordering = ["created time"] :just for admin
+admin.site.register(Post,PostAdmin)
--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from website.models import Contact
 # Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    empty_Value_display = "Empty"
+    list_display = ("name", "email","created_date")
+    list_filter = ("email",)
+    search_fields = ("name" , "massage")
+    
+admin.site.register(Contact, ContactAdmin)
+
