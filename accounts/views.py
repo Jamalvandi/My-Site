6cc47305fbd82147bf9f55370b50f6fcae5a721b
Jamalvandi <125701210+Jamalvandi@users.noreply.github.com>
2025-03-07 23:07:49 +0330

    some change in  authenticating
    write the auth with from django.contrib.auth.forms import AuthenticationForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,29 @@
 from django.shortcuts import render ,redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 def login_view(request):
     
-    if request.method == "POST":
-        if request.user.is_authenticated:
-            username = request.POST["username"]
-            password = request.POST["password"]    
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect("/")
-            else:
-                print("Authentication failed")
+        if not request.user.is_authenticated:
+            if request.method == "POST":
+                form = AuthenticationForm(request = request, data = request.POST)
+                if form.is_valid():
+                    username = form.cleaned_data.get("username")  # ✅ Correct
+                    password = form.cleaned_data.get("password")  # ✅ Correct
+                    user = authenticate(request, username=username, password=password)
+                    if user is not None:
+                        login(request, user)
+                        return redirect("/")
+            form = AuthenticationForm()
+            context = {"form" : form }
+            return render(request,"accounts/login.html" , context)
         else:
-            print("Fuke no")
-    return render(request,"accounts/login.html")
+            print("klklklklkk")
+            return redirect("/")
+
+            
+    
+
 
 def logout_view(request):
     pass
