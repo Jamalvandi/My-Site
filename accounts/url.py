716eab113e73713edcebd4833605589062cbbc76
Jamalvandi <125701210+Jamalvandi@users.noreply.github.com>
2025-03-09 16:38:41 +0330

    logout and ...
    -add logout
    -return login to default views
    -use decorators (login_required)

--- accounts/url.py
+++ accounts/url.py
@@ -8,9 +8,8 @@ urlpatterns =[
     # login
     # path("login", login_view, name = "login"),
     path('login' , login_view, name="login"),
-    
     # registrations
     path("register", register_view, name= "register"),
     # logout
-    # path("logout" ,logout_view , name= "logout"),
+    path("logout" ,logout_view , name= "logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render ,redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def login_view(request):
     
@@ -8,8 +9,8 @@ def login_view(request):
             if request.method == "POST":
                 form = AuthenticationForm(request = request, data = request.POST)
                 if form.is_valid():
-                    username = form.cleaned_data.get("username")  # ✅ Correct
-                    password = form.cleaned_data.get("password")  # ✅ Correct
+                    username = form.cleaned_data.get("username")
+                    password = form.cleaned_data.get("password")
                     user = authenticate(request, username=username, password=password)
                     if user is not None:
                         login(request, user)
@@ -18,15 +19,15 @@ def login_view(request):
             context = {"form" : form }
             return render(request,"accounts/login.html" , context)
         else:
-            print("klklklklkk")
             return redirect("/")
 
             
     
 
-
+@login_required
 def logout_view(request):
-    pass
+    logout(request)
+    return redirect("/")
 
 def register_view(request):
     return render(request, "accounts/register.html")
